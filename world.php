6eed799029bdd2079a2c8e487b05b144ad4f363b
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2020-11-14 15:51:54 -0500

    Exercise 3 complete

--- world.php
+++ world.php
@@ -1,12 +1,19 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
+if (isset($_GET['country'])){
+  $country=$_GET['country'];
+  $sql_country="SELECT * FROM countries WHERE name LIKE '%$country%';";
+  $stmt=$conn->query($sql_country);
+  
+}
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
@@ -15,3 +22,4 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+
