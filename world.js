a907a72545a05b625b499baf1b500fda505a09fc
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2020-11-14 23:22:39 -0500

    Exercise 5

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 window.onload = function () 
-{   const lookup = document.querySelector('#lookup');
+{   const lookup_country = document.querySelector('#lookup-country');
+    const lookup_cities=document.querySelector('#lookup-cities');
     var result = document.querySelector('#result');
     $.ajax({
       url: 'world.php',
@@ -8,18 +9,30 @@ window.onload = function ()
       {result.innerHTML=response;}
   
     }); 
-    lookup.addEventListener('click', function(event) {
+    lookup_country.addEventListener('click', function(event) {
         event.preventDefault();
         var search =document.getElementById("country").value;
         $.ajax({
           url: 'world.php',
           type: 'GET',
-          data: { "country": search},
+          data: { "country": search,"context":"countries"},
           success: function(response) 
           {result.innerHTML=response;}
       
         });         
     });
+    lookup_cities.addEventListener('click', function(event) {
+      event.preventDefault();
+      var search =document.getElementById("country").value;
+      $.ajax({
+        url: 'world.php',
+        type: 'GET',
+        data: { "country": search,"context":"cities"},
+        success: function(response) 
+        {result.innerHTML=response;}
+    
+      });         
+  });
 
     
 }
